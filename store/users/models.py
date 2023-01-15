40efccb211fab3668e87a48aa3a362325e313f12
aignatev-90 <aignatev-90@mail.ru>
2023-01-15 20:41:25 +0300

    fully modified to CBV

--- store/users/models.py
+++ store/users/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.mail import send_mail
+from django.shortcuts import reverse
+from django.conf import settings
+from django.utils.timezone import now
 
 class User(AbstractUser):
     image = models.ImageField(upload_to = 'users_images', blank = 'True', null = 'true')
@@ -17,10 +20,16 @@ class EmailVerification(models.Model):
         return f'EmailVerification object for {self.user.email}'
 
     def send_verification_email(self):
+        link = reverse('users:email_verification', kwargs={'email':self.user.email, 'code':self.code})
+        verification_link = f'{settings.DOMAIN_NAME}{link}'
+        subject = 'Подтверждение учетной записи'
+        message = 'Для подтверждения учетной записи для {} перейдите по ссылке {}'.format(self.user.email, verification_link)
         send_mail(
-            'Subject here',
-            'Test verification Email.',
-            'from@example.com',
-            [self.user.email],
+            subject=subject,
+            message=message,
+            from_email = settings.EMAIL_HOST_USER,
+            recipient_list= [self.user.email],
             fail_silently=False,
         )
+    def is_expired(self):
+        return True if now() >= self.expiration else False
--- store/users/views.py
+++ store/users/views.py
@@ -1,12 +1,13 @@
-from django.urls import reverse_lazy
+
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.base import TemplateView
 from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
-
 from common.views import TitleMixin
 
-from users.models import User
+from users.models import User, EmailVerification
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
 
@@ -16,21 +17,6 @@ class UserLoginView(TitleMixin, LoginView):
     form_class = UserLoginForm
     title = 'Store - Логин'
 
-# def login(request):
-#     if request.method == 'POST':
-#         form = UserLoginForm(data = request.POST)
-#         if form.is_valid():
-#             username = request.POST['username']
-#             password = request.POST['password']
-#             user = auth.authenticate(username=username, password=password)
-#             if user:
-#                 auth.login(request, user)
-#                 print('ok')
-#                 return HttpResponseRedirect(reverse('index'))
-#     else:
-#         form = UserLoginForm()
-#     context = {'form': form}
-#     return render(request, 'users/login.html', context)
 
 class UserRegistrationView(SuccessMessageMixin, TitleMixin, CreateView):
     model = User
@@ -48,16 +34,38 @@ class UserProfileView(TitleMixin, UpdateView):
     def get_success_url(self):
         return reverse_lazy('users:profile', args=(self.object.id,))
 
-    def get_context_data(self, **kwargs):
-        context = super(UserProfileView, self).get_context_data()
-        context['baskets'] = Basket.objects.filter(user=self.object)
-        return context
-
 class EmailVerificationView(TitleMixin, TemplateView):
     title = 'Store - Подтверждение электронной почты'
     template_name = 'users/email_verification.html'
 
+    def get(self, request, *args, **kwargs):
+        code = kwargs['code']
+        user = User.objects.get(email=kwargs['email'])
+        email_verifications = EmailVerification.objects.filter(user=user, code=code)
+        if email_verifications.exists() and not email_verifications.first().is_expired():
+            user.is_verified_email = True
+            user.save()
+            return super(EmailVerificationView, self).get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('index'))
 
+
+
+# def login(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(data = request.POST)
+#         if form.is_valid():
+#             username = request.POST['username']
+#             password = request.POST['password']
+#             user = auth.authenticate(username=username, password=password)
+#             if user:
+#                 auth.login(request, user)
+#                 print('ok')
+#                 return HttpResponseRedirect(reverse('index'))
+#     else:
+#         form = UserLoginForm()
+#     context = {'form': form}
+#     return render(request, 'users/login.html', context)
 
 
 # def registration(request):
